6866a4c1af3ccf8a311cb41a879c6fa9f23e8c8f
Do Nhat <dmnhat.pt@gmail.com>
2024-01-29 16:37:03 +0700

    Firebase CLI and SDK Setup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/screens/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
